4d2a7b888e59a56c859d1c8655c00b7dfdba0004
Andrey-SD <andrey.sd.post@gmail.com>
2017-11-03 12:33:18 +0200

    added two tables

--- app/Http/Controllers/Frontend/DefaultController.php
+++ app/Http/Controllers/Frontend/DefaultController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Frontend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\ProductsHasOrder;
 
 class DefaultController extends Controller
 {
 	public function index()
 	{
+		$temp = ProductsHasOrder::all();
+		dd($temp);
 		return view('default.index');
 	}
 }
