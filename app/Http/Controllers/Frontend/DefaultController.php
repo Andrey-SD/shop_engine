daff0e0f09e3c57eba0147e91f76643cbf52043a
Andrey-SD <andrey.sd.post@gmail.com>
2017-11-03 12:40:11 +0200

    returned not my files to their original state

--- app/Http/Controllers/Frontend/DefaultController.php
+++ app/Http/Controllers/Frontend/DefaultController.php
@@ -4,14 +4,11 @@ namespace App\Http\Controllers\Frontend;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\ProductsHasOrder;
 
 class DefaultController extends Controller
 {
 	public function index()
 	{
-		$temp = ProductsHasOrder::all();
-		dd($temp);
 		return view('default.index');
 	}
 }
